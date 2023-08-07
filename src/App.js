74b26f309d7e47ba223e9c59e98eec893fee4b3d
Jesus <6.jesus.a@gmail.com>
2023-08-07 04:55:33 -0400

    se realiza map y destructuracion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './componentes/Header/Header';
-import Formulario from './componentes/Formulario/Formulario';
-import MiOrg from './componentes/MiOrg';
+import { useState } from "react";
+import "./App.css";
+import Header from "./componentes/Header/Header";
+import Formulario from "./componentes/Formulario/Formulario";
+import MiOrg from "./componentes/MiOrg";
+import Equipo from "./componentes/Equipo";
 
 function App() {
-  const [mostrarFormulario,actualizarMostrar] = useState(true);
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
 
   const cambiarMostrar = () => {
-    actualizarMostrar(!mostrarFormulario)
-  }
+    actualizarMostrar(!mostrarFormulario);
+  };
+
+  // lista de equipos 
+  const equipos = [
+    {
+      titulo: "Programacion",
+      colorPrimario:"#57C278", 
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      titulo: "Front End",
+      colorPrimario:"#82CFFA", 
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario:"#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      titulo: "Devops",
+      colorPrimario:"#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      titulo: "UX y diseño",
+      colorPrimario:"#DB6EBF", 
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      titulo: "Movil",
+      colorPrimario:"#FFBA05", 
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      titulo: "Innovacion y gestion",
+      colorPrimario:"#FF8A29", 
+      colorSecundario: "#FFEEDF"
+    },
+  ];
   return (
-    <div >
-      <Header/>
+    <div>
+      <Header />
       {/* {mostrarFormulario === true ? <Formulario/> :<div/> } */}
       {/* {mostrarFormulario ? <Formulario/> :<></> } */}
       {/* Cortocircuitar */}
-      {mostrarFormulario &&<Formulario/> } 
-      <MiOrg cambiarMostrar={cambiarMostrar}/>
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+      {
+        equipos.map((equipo)=> <Equipo datos={equipo} key={equipo.titulo}/>)
+      }
     </div>
   );
 }
